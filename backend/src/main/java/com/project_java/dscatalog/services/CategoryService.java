10a07a4c4732462f51c76fc44aaa312e8bb6f3eb
kel2203 <raquelsb2001@gmail.com>
2023-05-11 20:40:13 -0300

    DTO repository

--- backend/src/main/java/com/project_java/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/project_java/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.project_java.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
 	
 	}
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+	}
+	
+	
 
 }
