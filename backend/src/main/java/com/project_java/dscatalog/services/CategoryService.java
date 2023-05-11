98aa85e2e0d3f62e1a3c0f32ca6f29f9152b4ff1
kel2203 <raquelsb2001@gmail.com>
2023-05-11 19:02:11 -0300

    H2 Database

---
+++ backend/src/main/java/com/project_java/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.project_java.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project_java.dscatalog.entities.Category;
+import com.project_java.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
