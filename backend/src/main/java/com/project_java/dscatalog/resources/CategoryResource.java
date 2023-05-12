f8aafeae2e1d37d6b9185f05d3dbb8a123664140
kel2203 <raquelsb2001@gmail.com>
2023-05-12 13:39:31 -0300

    Post Category

--- backend/src/main/java/com/project_java/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/project_java/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.project_java.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project_java.dscatalog.dto.CategoryDTO;
 import com.project_java.dscatalog.services.CategoryService;
@@ -31,4 +35,11 @@ public class CategoryResource {
 		CategoryDTO dto = service.findById(id);
 		 return ResponseEntity.ok().body(dto);	 
  }
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/project_java/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/project_java/dscatalog/services/CategoryService.java
@@ -31,6 +31,14 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+	
 	
 
 }
