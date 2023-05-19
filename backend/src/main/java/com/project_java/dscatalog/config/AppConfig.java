66f25ebb3c4615f6db577f852d98e8268022c719
kel2203 <raquelsb2001@gmail.com>
2023-05-19 12:18:38 -0300

    Custom validation response

--- backend/src/main/java/com/project_java/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/project_java/dscatalog/config/AppConfig.java
@@ -3,11 +3,12 @@ package com.project_java.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
 public class AppConfig {
 
-@Bean
-public BCryptPasswordEncoder passwordEncoder() {
-	return new BCryptPasswordEncoder();
-};
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
