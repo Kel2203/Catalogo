cb28aa489e9862549111c64fbc3c75bd013d9a40
kel2203 <raquelsb2001@gmail.com>
2023-06-01 13:47:31 -0300

    layout home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
-     <h1> Hello World </h1>
+    <Home />
     </>
   
   );
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+
+import{ReactComponent as MainImage} from 'assets/images/main-image.svg';
+import ButtonIcon from "components/ButtonIcon";
+import Navbar from "components/Navbar";
+
+import './styles.css';
+const Home = ()  =>{
+    return (
+      <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+        <div className="home-content-container">
+            <h1 className="home-title">Conheça o melhor catálogo de produtos</h1>
+            <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+            <ButtonIcon />
+        </div>
+        <div className="home-image-container">
+            <MainImage />
+        </div>
+        </div>
+
+      </div>
+      </>
+    
+    );
+  }
+  
+  export default Home;
