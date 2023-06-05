d0c35732cde699383bf289a139d60c78334d93d7
kel2203 <raquelsb2001@gmail.com>
2023-06-05 14:20:01 -0300

    token

---
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -0,0 +1,81 @@
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import './styles.css';
+
+
+import { getAuthData, requestBackendLogin, saveAuthData } from 'util/request';
+
+type FormData = {
+  username: string;
+  password: string;
+};
+
+const Login = () => {
+
+    const [hasError, setHasError] = useState(false);
+
+  const { register, handleSubmit, formState : {errors} } = useForm<FormData>();
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        saveAuthData(response.data);
+        setHasError(false);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
+  };
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register('username', {
+                required: 'Campo obrigatório',
+                pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email inválido'
+                }
+            })}
+            type="text"
+            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`} 
+            placeholder="Email"
+            name="username"
+          />
+          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+        </div>
+        <div className="mb-2">
+          <input
+            {...register('password', {required: 'Campo obrigatório'})}
+            type="password"
+            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
+            placeholder="Password"
+            name="password"
+          />
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/util/request.ts
+++ frontend/src/util/request.ts
@@ -1,1 +1,63 @@
+import axios from "axios";
+import qs from "qs";
+import { AxiosRequestConfig } from "axios";
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+const basicHeader = () => 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
+
+type LoginResponse = {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    scope: string;
+    userFirstName: string;
+    userId: number;
+}
+
+
+type LoginData = {
+    username: string;
+    password: string;
+}
+
+export const requestBackendLogin = (loginData : LoginData) => {
+   const headers = {
+    'Content-Type' : 'application/x-www-form-urlencoded',
+    Authorization: basicHeader()
+   }
+
+   const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password'
+   });
+
+   return axios({
+         method: 'POST',
+            baseURL: BASE_URL,  
+            url: '/oauth/token',
+            data,
+            headers
+    });
+}
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+    const headers = config.withCredentials
+      ? {
+          ...config.headers,
+          Authorization: 'Bearer ' + getAuthData().access_token,
+        }
+      : config.headers;
+  
+    return axios({ ...config, baseURL: BASE_URL, headers });
+  };
+
+export const saveAuthData = (obj : LoginResponse) => {
+    localStorage.setItem('authData', JSON.stringify(obj));
+}
+
+export const getAuthData = () => {
+    const str = localStorage.getItem('authData') ?? '{}';
+    return JSON.parse(str) as LoginResponse;
+}
