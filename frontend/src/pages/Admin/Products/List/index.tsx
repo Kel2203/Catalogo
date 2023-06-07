afee023a2fe4655258922983a0a1dbab9727683f
kel2203 <raquelsb2001@gmail.com>
2023-06-07 14:02:55 -0300

    CategoryBadge

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,4 +1,4 @@
-import ProductCrudCard from "components/ProductCrudCard";
+import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
 import './styles.css';
 import { Link } from "react-router-dom";
 const List = () => {
