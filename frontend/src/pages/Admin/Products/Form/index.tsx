a42b9227f2d420e33340123248e37f481ef7c947
kel2203 <raquelsb2001@gmail.com>
2023-06-07 15:51:26 -0300

    React hook form

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,63 +1,207 @@
+import { AxiosRequestConfig } from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import CurrencyInput from 'react-currency-input-field';
+import { useForm, Controller } from 'react-hook-form';
+import { useHistory, useParams } from 'react-router-dom';
+import Select from 'react-select';
+import { Category } from 'types/category';
+import { Product } from 'types/product';
+import { requestBackend } from 'util/request';
+import { toast } from 'react-toastify';
+
 import './styles.css';
 
+type UrlParams = {
+  productId: string;
+};
+
 const Form = () => {
+  const { productId } = useParams<UrlParams>();
+
+  const isEditing = productId !== 'create';
+
+  const history = useHistory();
+
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    control,
+  } = useForm<Product>();
+
+  useEffect(() => {
+    requestBackend({ url: '/categories' }).then((response) => {
+      setSelectCategories(response.data.content);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isEditing) {
+      requestBackend({ url: `/products/${productId}` }).then((response) => {
+        const product = response.data as Product;
+
+        setValue('name', product.name);
+        setValue('price', product.price);
+        setValue('description', product.description);
+        setValue('imgUrl', product.imgUrl);
+        setValue('categories', product.categories);
+      });
+    }
+  }, [isEditing, productId, setValue]);
+
+  const onSubmit = (formData: Product) => {
+    const data = {
+      ...formData,
+      price: String(formData.price).replace(',', '.'),
+    };
+
+    const config: AxiosRequestConfig = {
+      method: isEditing ? 'PUT' : 'POST',
+      url: isEditing ? `/products/${productId}` : '/products',
+      data,
+      withCredentials: true,
+    };
+
+    requestBackend(config)
+      .then(() => {
+        toast.info('Produto cadastrado com sucess');
+        history.push('/admin/products');
+      })
+      .catch(() => {
+        toast.error('Erro ao cadastrar produto');
+      });
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/products');
+  };
+
   return (
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
-        <h1 className="product-crud-form-title">Dados do produto</h1>
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-        <form action="">
-          <div className="row">
-            <div className="col-lg-6 product-crud-inputs-left">
-                <div className="margin-bottom-30">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
                 <input
-                type="text"
-                className="form-control base-input mb-5"
-                placeholder="Nome do produto"
-              />
-                </div>
-                <div className="margin-bottom-30">
-                <input
-                type="text"
-                className="form-control base-input mb-5"
-                placeholder="Nome do produto"
-              />
-                </div> 
-                <div>
-                <input
-                type="text"
-                className="form-control base-input mb-5"
-                placeholder="Nome do produto"
-              />
-                </div>
-             
-              <select className="form-control mb-5">
-                <option value="1">Livros</option>
-                <option value="2">Computadores</option>
-                <option value="3">Eletrônicos</option>
-              </select>
-              <input type="text" className="form-control" placeholder="Preço" />
-
-              <div className="col-lg-6">
-                <textarea
-                  name=""
-                  id=""
-                  cols={30}
-                  rows={10}
-                  className="form-control base-input mb-5"
-                  placeholder="Descrição"
-                ></textarea>
-                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
                   type="text"
-                  className="form-control mb-5"
-                  placeholder="Imagem do produto"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
                 />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
+              </div>
+
+              <div className="margin-bottom-30 ">
+                <Controller
+                  name="categories"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={selectCategories}
+                      classNamePrefix="product-crud-select"
+                      isMulti
+                      getOptionLabel={(category: Category) => category.name}
+                      getOptionValue={(category: Category) =>
+                        String(category.id)
+                      }
+                    />
+                  )}
+                />
+                {errors.categories && (
+                  <div className="invalid-feedback d-block">
+                    Campo obrigatório
+                  </div>
+                )}
+              </div>
+
+              <div className="margin-bottom-30">
+                <Controller
+                  name="price"
+                  rules={{ required: 'Campo obrigatório' }}
+                  control={control}
+                  render={({ field }) => (
+                    <CurrencyInput
+                      placeholder="Preço"
+                      className={`form-control base-input ${
+                        errors.name ? 'is-invalid' : ''
+                      }`}
+                      disableGroupSeparators={true}
+                      value={field.value}
+                      onValueChange={field.onChange}
+                    />
+                  )}
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.price?.message}
+                </div>
+              </div>
+
+              <div className="margin-bottom-30">
+                <input
+                  {...register('imgUrl', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                      message: 'Deve ser uma URL válida',
+                    },
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="URL da imagem do produto"
+                  name="imgUrl"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.imgUrl?.message}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  rows={10}
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Descrição"
+                  name="description"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
               </div>
             </div>
           </div>
-          <div className='product-crud-buttons-container'>
-            <button className="btn btn-outline-danger"> CANCELAR</button>
-            <button className="btn btn-primary text-white">SALVAR</button>
+          <div className="product-crud-buttons-container">
+            <button
+              className="btn btn-outline-danger product-crud-button"
+              onClick={handleCancel}
+            >
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
           </div>
         </form>
       </div>
--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,59 +1,51 @@
-import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import './styles.css';
-import { Link } from "react-router-dom";
-import { Product } from "types/product";
-import { useEffect, useState } from "react";
-import { requestBackend } from "util/request";
-import { AxiosRequestConfig } from "axios";
-import { SpringPage } from "types/vendor/spring";
+import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { useEffect, useState } from 'react';
+import { requestBackend } from 'util/request';
+import { AxiosRequestConfig } from 'axios';
+import { SpringPage } from 'types/vendor/spring';
 
 const List = () => {
-    const [page, setPage] = useState<SpringPage<Product>>();
-    useEffect(() => {
-        const config: AxiosRequestConfig = {
-          method: 'GET',
-          url: "/products",
-          params: {
-            page: 0,
-            size: 50,
-          }
-        };
-        requestBackend(config)
-          .then((response) => {
-            setPage(response.data);
-          });
-      }, []);
-    
-    return (
-        <div className="product-crud-container">
-        <div  className="product-crud-bar-container" >
-            <Link to="/admin/products/create">
-            <button className="btn btn-primary text-white btn-crud-add">
+  const [page, setPage] = useState<SpringPage<Product>>();
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const getProducts = () => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+      },
+    };
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  };
+  return (
+    <div className="product-crud-container">
+      <div className="product-crud-bar-container">
+        <Link to="/admin/products/create">
+          <button className="btn btn-primary text-white btn-crud-add">
             Adicionar
-           </button>
-            </Link>
-           
-           <div className="base-card product-filter-container">
-            Search bar
-           </div>
-           </div>
+          </button>
+        </Link>
 
-           <div className="row">
+        <div className="base-card product-filter-container">Search bar</div>
+      </div>
 
-            {
-                page?.content.map(product => (
-                    <div key={product.id} className="col-sm-6 col-md-12">
-                    <ProductCrudCard product={product} />
-                    </div>
-                ))
-
-            }
-
-
-          
-           </div>
-           </div>
-    )
-}
+      <div className="row">
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default List;
