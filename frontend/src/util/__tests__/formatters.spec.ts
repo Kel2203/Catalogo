5221401a9467a2e1fd3f83e9674c2cf2f94e4f54
kel2203 <raquelsb2001@gmail.com>
2023-06-09 14:20:24 -0300

    First Test

---
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -0,0 +1,12 @@
+import { formatPrice } from "util/formatters";
+
+test('formatPrice should number pt-BR when given 10.1', () => {
+ 
+
+    //ACT
+    const result = formatPrice(10.1);
+
+    //ASSERT
+    expect(result).toEqual("10,10");
+
+} )
