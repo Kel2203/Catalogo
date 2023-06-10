9c681a3044e411eda99e8d4a62b8537aec147929
kel2203 <raquelsb2001@gmail.com>
2023-06-10 13:40:28 -0300

    Pagination click tests

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -27,8 +27,8 @@ const Pagination = ( { forcePage, pageCount, range, onChange } : Props) => {
 
       onPageChange={(items) => (onChange) ? onChange(items.selected) : {}}
 
-      previousLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
-      nextLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
+      previousLabel={<div className="pagination-arrow-container" data-testid="arrow-previous"><ArrowIcon /></div>}
+      nextLabel={<div className="pagination-arrow-container" data-testid="arrow-next"><ArrowIcon /></div>}
     />
   );
 };
