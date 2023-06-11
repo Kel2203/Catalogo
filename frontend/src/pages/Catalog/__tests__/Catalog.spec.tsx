7ef674dec1efaa66f99d5aa61b69782b091ef0da
kel2203 <raquelsb2001@gmail.com>
2023-06-11 13:44:09 -0300

    MSW mock server, fixtures

--- frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,7 +2,13 @@ import { render, screen, waitFor } from "@testing-library/react";
 import Catalog from "..";
 import { Router } from 'react-router-dom';
 import history from 'util/history';
+import {server} from './fixtures'
 
+
+beforeAll(() => {server.listen()})
+afterEach(() => {server.resetHandlers()})
+afterAll(() => {server.close()})
+  
 test(' should render Catalog with products', async () => {
    //Arrange
   
